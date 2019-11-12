59768b3aa8c34567008071905b6315e397a84b4e
Sabine Brunner <sabine.brunner@iwi.unibe.ch>
2019-11-12 16:25:56 +0100

    add language to config

--- src/app/config/services/config.service.ts
+++ src/app/config/services/config.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Config } from '../config';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { LanguageService } from '../../data/services/language.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
-  public config : Config;
-  private subject : BehaviorSubject<Config>
+  public config: Config;
+  private subject: BehaviorSubject<Config>;
+  private _langSub: Subscription;
 
-  constructor() { 
+  constructor(
+    private langService: LanguageService,
+  ) {
     this.setup();
   }
 
   public setup() {
     this.config = new Config();
     this.subject = new BehaviorSubject(this.config);
+    this._langSub = this.langService.currentLang.subscribe(
+      lang => {
+        this.config.language = lang;
+      });
   }
 
-  public set(attribute : string, value : any) {
+  public set(attribute: string, value: any) {
     this.config[attribute] = value;
     this.subject.next(this.config);
   }
 
-  public get(attribute : string) {
+  public get(attribute: string) {
     return this.config[attribute];
   }
 
